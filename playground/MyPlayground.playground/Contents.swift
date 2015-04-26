e71ddabdc77dd61327079f50a0b948fc99d37aea
Karlkim Suwamongkol <contact@karlk.im>
2015-04-26 00:13:10 -0700

    Class and Objects

--- playground/MyPlayground.playground/Contents.swift
+++ playground/MyPlayground.playground/Contents.swift
@@ -129,10 +129,52 @@ let ret = numbers.map({number in return 4 * number})
 ret
 
 
+class Counter {
+    var count: Int = 0
+    func incrementBy(amount: Int, numberOfTimes times: Int) {
+        count += amount * times
+    }
+    
+}
+var counter = Counter()
+counter.incrementBy(2, numberOfTimes: 7)
 
 
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
 
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() ->  Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
 
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let nilSquare: Square? = nil
+optionalSquare?.sideLength
+nilSquare?.sideLength
 
 
 
