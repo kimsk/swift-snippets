f1df2175e65a68f71be9f6c38303f28b3e3d4adf
Karlkim Suwamongkol <contact@karlk.im>
2015-04-25 23:57:01 -0700

    try swift

---
+++ playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,144 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+println("Hello, world!")
+
+let something = "Test"
+let another = 1
+println(String(another) + something)
+
+var value:Float = 4.0
+
+let apples = 3
+let appleSummary = "I have \(apples) apples."
+
+var arrayInt = [1,2,3]
+arrayInt[0]
+
+var emptyArray = [String]()
+println(emptyArray)
+emptyArray.append("TEST")
+emptyArray[0] = "TEST2"
+emptyArray.append("XXX")
+emptyArray.capacity
+emptyArray.count
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+println(teamScore)
+
+var optionalString: String? = "Hello"
+println(optionalString == nil)
+
+var optionalName: String? = nil
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+optionalString?.isEmpty
+
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    let vegetableComment = "Add some raisins and make ants on a log."
+case "cucumber", "watercress":
+    let vegetableComment = "That would make a good tea sandwich."
+case let x where x.hasSuffix("pepper"):
+    let vegetableComment = "Is it a spicy \(x)?"
+default:
+    let vegetableComment = "Everything tastes good in soup."
+}
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics([5, 3, 100, 3, 9])
+println(statistics.sum)
+println(statistics.2)
+
+
+func returnZeroFromOdd() -> (Int -> Int) {
+    
+    func fun(number:Int) -> Int{
+        if number%2 != 0 {
+            return 0
+        } else {
+            return number
+        }
+    }
+    
+    return fun
+}
+
+func returnZeroFromOdd2(number:Int) -> Int{
+    if number%2 != 0 {
+        return 0
+    } else {
+        return number
+    }
+}
+
+var numbers = [20, 19, 7, 12]
+
+numbers.map(returnZeroFromOdd())
+numbers.map(returnZeroFromOdd2)
+
+
+numbers.map({
+    (number: Int) -> Int in
+        let result = 3 * number
+        return result
+    })
+
+
+numbers.map({
+    (number) -> Int in
+        if number%2 != 0 {
+            return 0
+        }
+        else {
+            return number
+        }
+    })
+
+let ret = numbers.map({number in return 4 * number})
+ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
